d408818007b26031edf0fd28dfe37571cdc3df5a
TariqSarfaraz <tariqsarfaraz7@gmail.com>
2022-02-06 23:15:57 +0530

    deleted

--- Phase1/src/accessmodifiers/ProtectedAccessModifier.java
+++
@@ -1,9 +0,0 @@
-package accessmodifiers;
-
-public class ProtectedAccessModifier {
-
-	protected void protectedMethod() {
-
-		System.out.println("Displaying Protected Method.");
-	}
-}
--- Phase1/src/accessmodifiers/PublicAccessModifier.java
+++
@@ -1,9 +0,0 @@
-package accessmodifiers;
-
-public class PublicAccessModifier {
-
-	public void publicMethod() {
-
-		System.out.println("Displaying Publice Method.");
-	}
-}
